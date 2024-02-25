fac9303ab1daa8b4e188673f75051778690d0376
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-02-25 09:30:55 -0500

    server: implement buffer system
    Ensuring that all buffer wl_resources have the same implementation
    allows for type-checking them (see `server_buffer_from_resource`).
    
    Individual buffer implementations (linux_dmabuf, wl_shm) can provide
    their own destructor functions in the call to
    `wl_resource_set_implementation`.
    
    Invalid buffers (type SERVER_BUFFER_INVALID) need to exist for some
    buffer factories. For example, linux_dmabuf's `create_immed` method can
    return an "invalid buffer" whose behavior is compositor-defined.

---
+++ include/server/buffer.h
@@ -0,0 +1,29 @@
+#ifndef WAYWALL_SERVER_BUFFER_H
+#define WAYWALL_SERVER_BUFFER_H
+
+#include <wayland-client.h>
+#include <wayland-server.h>
+
+struct server_buffer {
+    struct wl_resource *resource;
+    struct wl_buffer *remote;
+
+    enum server_buffer_type {
+        SERVER_BUFFER_SHM,
+        SERVER_BUFFER_DMABUF,
+        SERVER_BUFFER_INVALID,
+    } type;
+    union {
+        // TODO: shm data
+        // TODO: dmabuf data
+    } data;
+};
+
+extern const struct wl_buffer_listener server_buffer_listener;
+extern const struct wl_buffer_interface server_buffer_impl;
+
+int server_buffer_create_invalid(struct wl_resource *resource);
+void server_buffer_free(struct server_buffer *buffer);
+struct server_buffer *server_buffer_from_resource(struct wl_resource *resource);
+
+#endif
---
+++ waywall/server/buffer.c
@@ -0,0 +1,71 @@
+#include "server/buffer.h"
+#include "util.h"
+#include <stdlib.h>
+#include <wayland-client.h>
+
+static void
+on_buffer_release(void *data, struct wl_buffer *wl) {
+    struct server_buffer *buffer = data;
+
+    wl_buffer_send_release(buffer->resource);
+}
+
+const struct wl_buffer_listener server_buffer_listener = {
+    .release = on_buffer_release,
+};
+
+static void
+buffer_destroy(struct wl_client *client, struct wl_resource *resource) {
+    wl_resource_destroy(resource);
+}
+
+const struct wl_buffer_interface server_buffer_impl = {
+    .destroy = buffer_destroy,
+};
+
+static void
+invalid_buffer_destroy(struct wl_resource *resource) {
+    struct server_buffer *buffer = wl_resource_get_user_data(resource);
+    server_buffer_free(buffer);
+}
+
+int
+server_buffer_create_invalid(struct wl_resource *resource) {
+    struct server_buffer *buffer = calloc(1, sizeof(*buffer));
+    if (!buffer) {
+        struct wl_client *client = wl_resource_get_client(resource);
+        wl_client_post_no_memory(client);
+        return 1;
+    }
+
+    buffer->resource = resource;
+    buffer->type = SERVER_BUFFER_INVALID;
+
+    wl_resource_set_implementation(resource, &server_buffer_impl, buffer, invalid_buffer_destroy);
+
+    return 0;
+}
+
+void
+server_buffer_free(struct server_buffer *buffer) {
+    if (buffer->remote) {
+        wl_buffer_destroy(buffer->remote);
+    }
+
+    switch (buffer->type) {
+    case SERVER_BUFFER_SHM:
+        break;
+    case SERVER_BUFFER_DMABUF:
+        break;
+    case SERVER_BUFFER_INVALID:
+        return;
+    }
+
+    free(buffer);
+}
+
+struct server_buffer *
+server_buffer_from_resource(struct wl_resource *resource) {
+    ww_assert(wl_resource_instance_of(resource, &wl_buffer_interface, &server_buffer_impl));
+    return wl_resource_get_user_data(resource);
+}
