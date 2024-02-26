9a1a3cb7e3eff0d1e45465e7a9467d699ebc3657
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-02-26 17:41:33 -0500

    main: handle SIGINT, shutdown gracefully

--- waywall/main.c
+++ waywall/main.c
@@ -1,5 +1,14 @@
 #include "server/server.h"
 #include "util.h"
+#include <signal.h>
+#include <wayland-server-core.h>
+
+static int
+handle_signal(int signal, void *data) {
+    struct server *server = data;
+    server_shutdown(server);
+    return 0;
+}
 
 int
 main() {
@@ -7,5 +16,16 @@ main() {
     if (!server) {
         return 1;
     }
+
+    struct wl_event_loop *loop = wl_display_get_event_loop(server->display);
+    struct wl_event_source *src_sigint =
+        wl_event_loop_add_signal(loop, SIGINT, handle_signal, server);
+
+    wl_display_run(server->display);
+
+    wl_event_source_remove(src_sigint);
+    server_destroy(server);
+
+    ww_log(LOG_INFO, "Done");
     return 0;
 }
