e43d18e749f1aa3d40f8fbc82958c3ded9e36c81
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-03-24 16:40:54 -0400

    tools: add waywall-launch
    Currently, this just sets WAYLAND_DISPLAY and executes the provided
    command. The next step is to use systemd-run (if available) to place the
    new instance inside of the waywall cgroup. At some point in the future,
    I would like to figure out how to prevent environment variables from
    propagating to child processes in order to ensure that any apps spawned
    by Minecraft go back out to the host session.

---
+++ tools/waywall-launch.c
@@ -0,0 +1,43 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static int
+print_help(int argc, char **argv) {
+    fprintf(stderr, "USAGE: %s COMMAND [ARGS...]\n", argc ? argv[0] : "waywall-launch");
+    return 1;
+}
+
+int
+main(int argc, char **argv) {
+    if (argc < 2) {
+        return print_help(argc, argv);
+    }
+
+    int fd = open("/tmp/waywall-display", O_RDONLY);
+    if (fd == -1) {
+        if (errno == ENOENT) {
+            fprintf(stderr, "waywall is not running");
+        } else {
+            perror("failed to open waywall-display");
+        }
+        return 1;
+    }
+
+    char buf[4096];
+    ssize_t n = read(fd, buf, sizeof(buf) - 1);
+    if (n == -1) {
+        perror("failed to read waywall-display");
+        return 1;
+    }
+    buf[n] = '\0';
+    close(fd);
+
+    setenv("WAYLAND_DISPLAY", buf, 1);
+
+    execvp(argv[1], &argv[1]);
+    perror("execvp failed");
+    return 1;
+}
