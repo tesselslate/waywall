166e939d752e2b1b96c8770cc5f5ad29013f73e2
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-04-27 22:55:13 -0400

    util: add pidfd_open and pidfd_send_signal
    These are needed for `waywall wrap` to effectively manage child
    processes.

--- include/util/syscall.h
+++ include/util/syscall.h
@@ -1,6 +1,11 @@
 #ifndef WAYWALL_UTIL_SYSCALL_H
 #define WAYWALL_UTIL_SYSCALL_H
 
+#include <signal.h>
+#include <sys/types.h>
+
 int memfd_create(const char *name, unsigned int flags);
+int pidfd_open(pid_t pid, unsigned int flags);
+int pidfd_send_signal(int pidfd, int sig, siginfo_t *info, unsigned int flags);
 
 #endif
--- waywall/util/syscall.c
+++ waywall/util/syscall.c
@@ -2,9 +2,20 @@
 
 #include "util/syscall.h"
 #include <sys/syscall.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int
 memfd_create(const char *name, unsigned int flags) {
     return syscall(SYS_memfd_create, name, flags);
+}
+
+int
+pidfd_open(pid_t pid, unsigned int flags) {
+    return syscall(SYS_pidfd_open, pid, flags);
+}
+
+int
+pidfd_send_signal(int pidfd, int sig, siginfo_t *info, unsigned int flags) {
+    return syscall(SYS_pidfd_send_signal, pidfd, sig, info, flags);
 }
