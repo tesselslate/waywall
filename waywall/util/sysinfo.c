3ac0ca37bff0094a7bcfa248059ab60733045617
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-10-04 13:52:30 -0400

    counter: remove reset counter module
    Somehow I forgot to remove this when removing wall code. It's gone now.

--- waywall/util/sysinfo.c
+++ waywall/util/sysinfo.c
@@ -15,7 +15,7 @@
 #define PATH_INOTIFY_MAX_USER_INSTANCES PATH_SYSCTL "fs/inotify/max_user_instances"
 #define PATH_INOTIFY_MAX_USER_WATCHES PATH_SYSCTL "fs/inotify/max_user_watches"
 
-long
+static long
 number_from_file(const char *path) {
     int fd = open(path, O_RDONLY);
     if (fd == -1) {
