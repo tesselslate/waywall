af410cc37c7f32385f7da4293247ec6fc9e1d756
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-11-01 22:26:25 -0400

    wrap: put instance beneath scene objects

--- waywall/wrap.c
+++ waywall/wrap.c
@@ -368,6 +368,11 @@ on_view_create(struct wl_listener *listener, void *data) {
     server_view_set_visible(wrap->view, true);
     server_view_commit(wrap->view);
 
+    // HACK: This is so that scene objects (images, mirrors, text) appear over the instance. This is
+    // probably not the best spot to do it, though.
+    wl_subsurface_place_below(wrap->view->subsurface, wrap->view->ui->tree.surface);
+    wl_surface_commit(wrap->view->surface->remote);
+
     server_gl_set_capture(wrap->gl, view->surface);
 }
 
