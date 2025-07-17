d523c95e2ad11ad4fa8f4eb3db49b5e80b6b6107
tesselslate <46545045+tesselslate@users.noreply.github.com>
2025-07-16 22:32:15 -0400

    ci: echo mdbook download URL

--- ci/install_mdbook.sh
+++ ci/install_mdbook.sh
@@ -2,6 +2,8 @@
 
 MDBOOK_TAG=$(curl "https://api.github.com/repos/rust-lang/mdbook/releases/latest" | jq -r ".tag_name")
 MDBOOK_URL="https://github.com/rust-lang/mdbook/releases/download/$MDBOOK_TAG/mdbook-$MDBOOK_TAG-x86_64-unknown-linux-gnu.tar.gz"
+
 mkdir mdbook
+echo "downloading mdbook from $MDBOOK_URL"
 curl -sSL $MDBOOK_URL | tar -xz --directory=./mdbook
 echo $(pwd)/mdbook >> $GITHUB_PATH
