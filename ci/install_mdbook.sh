aea67c1f1f7ff5075de9abe5c9c18e2ff706641b
tesselslate <46545045+tesselslate@users.noreply.github.com>
2025-12-20 17:10:22 -0500

    ci: update mdbook to 0.5.2

--- ci/install_mdbook.sh
+++ ci/install_mdbook.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 mkdir mdbook
-curl -sSL "https://github.com/rust-lang/mdbook/releases/download/v0.4.52/mdbook-v0.4.52-x86_64-unknown-linux-gnu.tar.gz" \
+curl -sSL "https://github.com/rust-lang/mdbook/releases/download/v0.5.2/mdbook-v0.5.2-x86_64-unknown-linux-gnu.tar.gz" \
     | tar -xz --directory=./mdbook
 echo $(pwd)/mdbook >> $GITHUB_PATH
