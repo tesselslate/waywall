0991437c7ca12c3a20dab8fdcbfb5da646a489a1
tesselslate <46545045+tesselslate@users.noreply.github.com>
2025-12-30 17:29:54 -0500

    util: add str
    This is intended to serve a similar purpose to Rust's str or C++'s
    std::string_view.

--- include/util/str.h
+++ include/util/str.h
@@ -2,20 +2,44 @@
 
 #include <sys/types.h>
 
+#define str_lit(lit) ((str){sizeof(lit) - 1, lit})
+
+typedef struct {
+    ssize_t len;
+    char *data;
+} str;
+
 typedef struct {
     ssize_t len, cap;
     char *data;
 } strbuf;
 
+struct strs {
+    ssize_t len;
+    str *data;
+};
+
 #define strbuf_append(dst, src)                                                                    \
     _Generic((src),                                                                                \
         int: strbuf_append_char,                                                                   \
         strbuf: strbuf_append_buf,                                                                 \
+        str: strbuf_append_str,                                                                    \
         default: strbuf_append_cstr)(dst, src)
 
 void strbuf_append_char(strbuf *dst, char src);
 void strbuf_append_cstr(strbuf *dst, const char *src);
 void strbuf_append_buf(strbuf *dst, strbuf src);
+void strbuf_append_str(strbuf *dst, str src);
 void strbuf_clear(strbuf *buf);
 void strbuf_free(strbuf *buf);
 strbuf strbuf_new();
+str strbuf_view(strbuf buf);
+
+strbuf str_clone(str s);
+bool str_eq(str a, str b);
+ssize_t str_index(str s, char needle, ssize_t start);
+str str_slice(str s, ssize_t start, ssize_t end);
+struct strs str_split(str s, char sep);
+
+void strs_free(struct strs strs);
+ssize_t strs_index(struct strs strs, str s, ssize_t start);
--- waywall/util/str.c
+++ waywall/util/str.c
@@ -1,9 +1,13 @@
 #include "util/str.h"
 #include "util/alloc.h"
+#include "util/list.h"
 #include <stdbit.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+
+LIST_DEFINE(str, list_str);
+LIST_DEFINE_IMPL(str, list_str);
 
 static constexpr ssize_t STRBUF_BASE_CAP = 64;
 
@@ -21,6 +25,14 @@ grow(strbuf *buf, ssize_t cap) {
     check_alloc(buf->data);
 
     memset(buf->data + buf->len, 0, buf->cap - buf->len);
+}
+
+static inline struct strs
+strs_from_list(struct list_str list) {
+    return (struct strs){
+        .len = list.len,
+        .data = list.data,
+    };
 }
 
 void
@@ -48,6 +60,14 @@ strbuf_append_buf(strbuf *dst, strbuf src) {
 }
 
 void
+strbuf_append_str(strbuf *dst, str src) {
+    grow(dst, dst->len + src.len);
+
+    memcpy(dst->data + dst->len, src.data, src.len);
+    dst->len += src.len;
+}
+
+void
 strbuf_clear(strbuf *buf) {
     buf->len = 0;
 }
@@ -66,4 +86,86 @@ strbuf_new() {
         .data = zalloc(STRBUF_BASE_CAP, 1),
     };
     return buf;
+}
+
+str
+strbuf_view(strbuf buf) {
+    return (str){buf.len, buf.data};
+}
+
+strbuf
+str_clone(str s) {
+    // include space for null terminator
+    ssize_t cap = stdc_bit_ceil((size_t)(s.len + 1));
+    char *data = zalloc(cap, 1);
+    memcpy(data, s.data, s.len);
+
+    return (strbuf){
+        .len = s.len,
+        .cap = cap,
+        .data = data,
+    };
+}
+
+bool
+str_eq(str a, str b) {
+    if (a.len != b.len) {
+        return false;
+    }
+
+    return (memcmp(a.data, b.data, a.len) == 0);
+}
+
+ssize_t
+str_index(str s, char needle, ssize_t start) {
+    char *ptr = memchr(s.data + start, needle, s.len - start);
+
+    return (ptr ? ptr - s.data : -1);
+}
+
+str
+str_slice(str s, ssize_t start, ssize_t end) {
+    ww_assert(start >= 0);
+    ww_assert(start <= end);
+    ww_assert(end <= s.len);
+
+    return (str){end - start, s.data + start};
+}
+
+struct strs
+str_split(str s, char sep) {
+    struct list_str list = list_str_create();
+
+    for (ssize_t pos = 0; pos < s.len;) {
+        ssize_t next = str_index(s, sep, pos);
+        if (next == -1) {
+            next = s.len;
+        }
+
+        list_str_append(&list, str_slice(s, pos, next));
+
+        pos = next + 1;
+
+        if (pos == s.len) {
+            list_str_append(&list, str_slice(s, s.len, s.len));
+        }
+    }
+
+    return strs_from_list(list);
+}
+
+void
+strs_free(struct strs strs) {
+    free(strs.data);
+}
+
+ssize_t
+strs_index(struct strs strs, str s, ssize_t start) {
+    for (ssize_t i = start; i < strs.len; i++) {
+        if (str_eq(strs.data[i], s)) {
+            return i;
+        }
+    }
+
+    return -1;
 }
